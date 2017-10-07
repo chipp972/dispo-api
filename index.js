7efd2adf7bec9573be36396001d5becf7c273385
Pierre-Charles Nicolas <pierrecharles.nicolas@gmail.com>
2017-10-07 19:12:41 +0200

    test websockets

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const http = require('http');
+const io = require('socket.io');
+
 const app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
+app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', (request, response) => {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+const server = http.Server(app);
+server.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
+});
+
+const ws = io(server);
+ws.on('connection', socket => {
+  socket.emit('news', { hello: 'world' });
+
+  socket.on('ping', (data) => {
+    console.log('pong')
+  })
 });
